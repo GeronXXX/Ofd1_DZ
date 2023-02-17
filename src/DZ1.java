6935a1292a60f139028c1965bba0c0d9c53f4cb2
ivan <lityagin.ru@gmail.com>
2023-02-17 17:05:37 +0300

    v3

--- src/DZ1.java
+++ src/DZ1.java
@@ -5,15 +5,15 @@ public class DZ1 {
         byte value2 = 3;
         byte value3 = (byte) (value1 + value2);
 
-        int value = 5;
-        int valueNew = 3;
+        int value = 122;
+        int valueNew = 18;
         value = --value;
         value = value--;
         int val = 5/2;
         int val1 = 5%2;
         double val2 = 5/2;
         float val3 = 5.2F;
-        int value4 = 5;
+        int value4 = 7;
 
         boolean c = (value > valueNew);
         boolean b = (value == value4);
